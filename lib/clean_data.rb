dd694fd5f9a3c31801539a4be035883c7055e06a
Evgeniy Maslov <eum@anahoret.com>
2015-08-07 13:32:52 +0300

    add functionality

--- lib/clean_data.rb
+++ lib/clean_data.rb
@@ -1,5 +1,19 @@
 require "clean_data/version"
 
 module CleanData
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  included do
+    before_validation :cleanup_data
+  end
+
+  private
+  def cleanup_data
+    # trim whitespace from beginning and end of string attributes
+    attribute_names.each do |name|
+      if send(name).respond_to?(:strip)
+        send("#{name}=", send(name).strip)
+      end
+    end
+  end
 end
